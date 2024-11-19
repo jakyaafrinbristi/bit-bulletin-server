5ff8cd883f812467782d5261fa2b47eb6526c823
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-11-19 18:52:20 +0600

    basic-project-setup-done

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from './App';
+import config from './app/config';
+import mongoose from 'mongoose';
+
+// const mongoose = require('mongoose');
+
+// main().catch(err => console.log(err));
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
