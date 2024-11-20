de35c9cdd1cd6ac86039cf0b2be700726fceec87
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-11-20 21:27:20 +0600

    done-students-create-feature

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,20 @@
 // const express = require('express')
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+//application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
   res.send('Hello World!');
-});
+};
+
+app.get('/', getAController);
 // console.log(process.cwd())
 export default app;
